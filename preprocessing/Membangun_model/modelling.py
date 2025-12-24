dd2a5c034a36bfdd1eeb1c5878db60d0feeb0fc8
Gilang Putra Firmansyah <124638907+Geversonvx@users.noreply.github.com>
2025-12-24 18:21:21 +0700

    Update modelling.py

--- preprocessing/Membangun_model/modelling.py
+++ preprocessing/Membangun_model/modelling.py
@@ -2,17 +2,23 @@ import pandas as pd
 import mlflow
 import mlflow.sklearn
 import dagshub
+
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+# =========================
+# Init DagsHub + MLflow
+# =========================
 dagshub.init(
     repo_owner="Geversonvx",
     repo_name="Eksperimen_SML_GilangPutraFirmansyah",
     mlflow=True,
 )
 
+
 def main():
     # =========================
-    # Load dataset hasil preprocessing
+    # Load data preprocessing
     # =========================
     X_train = pd.read_csv("CreditCardDefaultDataset_preprocessing/X_train.csv")
     X_test = pd.read_csv("CreditCardDefaultDataset_preprocessing/X_test.csv")
@@ -24,23 +30,20 @@ def main():
     ).values.ravel()
 
     # =========================
-    # Aktifkan MLflow autolog
+    # MLflow experiment
     # =========================
-    mlflow.set_tracking_uri("http://127.0.0.1:5000")
     mlflow.set_experiment("Credit_Card_Default_Basic_Model")
 
-    mlflow.sklearn.autolog()
-
-    # =========================
-    # Training
-    # =========================
     with mlflow.start_run():
+        # =========================
+        # Training
+        # =========================
         model = RandomForestClassifier(n_estimators=100, random_state=42, n_jobs=-1)
 
         model.fit(X_train, y_train)
 
         # =========================
-        # Evaluasi
+        # Evaluation
         # =========================
         y_pred = model.predict(X_test)
 
@@ -49,10 +52,27 @@ def main():
         rec = recall_score(y_test, y_pred)
         f1 = f1_score(y_test, y_pred)
 
-        print("Accuracy :", acc)
-        print("Precision:", prec)
-        print("Recall   :", rec)
-        print("F1-score :", f1)
+        # =========================
+        # Manual logging (WAJIB)
+        # =========================
+        mlflow.log_param("model_type", "RandomForest")
+        mlflow.log_param("n_estimators", 100)
+
+        mlflow.log_metric("accuracy", acc)
+        mlflow.log_metric("precision", prec)
+        mlflow.log_metric("recall", rec)
+        mlflow.log_metric("f1_score", f1)
+
+        # =========================
+        # Log & REGISTER model
+        # =========================
+        mlflow.sklearn.log_model(
+            sk_model=model,
+            artifact_path="model",
+            registered_model_name="CreditCardDefault_RF_Model",
+        )
+
+        print("Model trained & registered successfully")
 
 
 if __name__ == "__main__":
